badce4c0c5dcc6ae8ac4dda4efa16158d8d189ae
Arya Krishna <dr.aryakris@outlook.com>
2022-01-20 22:20:12 -0800

    initial files like server.js and npm downloads

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+app.use(express.static('public'));
+
+
+app.get('/note', (req,res) =>{
+    //send the file note.html
+})
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
