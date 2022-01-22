14e52bbb1864339cba6e4bdff390af0d82428559
Arya Krishna <dr.aryakris@outlook.com>
2022-01-22 09:46:57 -0800

    res.sendFile to send the files note.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const path = require ('path')
 const app = express();
 const PORT = 3001;
 app.use(express.static('public'));
 
 
-app.get('/note', (req,res) =>{
-    //send the file note.html
-})
+app.get('/notes', (req,res) =>{
+    //send the file notes.html
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+    //send the file notes.html
+    res.json()
+});
+app.post('api/notes' , (req,res) => {
+//Access the note data
+
+const newNote = req.body;
+
+
+
+
+
+    //create persistent data
+    res.json('a message')
+
+});
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
