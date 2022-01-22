cf59edbdf1d7113f89fb18eed62d9f525a29a722
Arya Krishna <dr.aryakris@outlook.com>
2022-01-22 10:01:07 -0800

    updating const port to the code to work in heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require ('path')
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
+const notes = 
 
 
 app.get('/notes', (req,res) =>{
